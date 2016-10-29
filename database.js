ef0f05d3e03bc76c0cbe37b50d6115250ff331f4
Gonzalo Bahamondez <gonzalo@becual.com>
2016-10-28 23:54:50 -0300

    database connection

---
+++ database.js
@@ -0,0 +1,34 @@
+const pg = require('pg');
+
+var config = {
+  user: 'postgres',
+  database: 'blog',
+  password: '',
+  host: 'localhost', 
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000,
+};
+
+var pool = new pg.Pool(config);
+
+pool.connect( function (err, client, done) {
+	if (err) {
+		console.log('error -> ', err);
+		return;
+	}
+
+	client.query('SELECT * FROM post', function (err, result) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		console.log('resultado de la consulta --> ', result.rows);
+		
+
+		client.end(function (err) {
+	      if (err) throw err;
+	    });
+	})
+});
--- database.sql
+++ database.sql
@@ -6,3 +6,5 @@ create table post(
   created_at TIMESTAMP,
   updated_at TIMESTAMP
 );
+
+INSERT INTO post (content, created_at, updated_at) VALUES ('Hola como estas', current_timestamp, current_timestamp);
